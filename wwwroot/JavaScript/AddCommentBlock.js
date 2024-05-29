8d7385790d1ccc994aac5e4489045616e81cb9b5
Przemek <przemekchelski@gmail.com>
2024-05-29 17:11:49 +0200

    fixed frontend of comments

--- wwwroot/JavaScript/AddCommentBlock.js
+++ wwwroot/JavaScript/AddCommentBlock.js
@@ -6,18 +6,28 @@
         var newDiv = document.createElement('div');
         newDiv.classList.add('Comment-Block');
         newDiv.id = "CommentBlock-" + PostId;
-        PostDiv.appendChild(newDiv);
-
+        PostDiv.appendChild(newDiv); 
+        
+        var CloseButtonContainer = document.createElement("div");
+        CloseButtonContainer.className = "CloseButton-Container";
+        
         var CloseButton = document.createElement("button");
         CloseButton.onclick = function () { ShoutCommentBlock("CommentBlock-" + PostId); };
         CloseButton.className = "CloseButton";
         CloseButton.id = "CloseButton-" + PostId;
-        newDiv.appendChild(CloseButton);
+        CloseButton.style.backgroundImage = "/Icons/CloseIcon.png";
+        newDiv.appendChild(CloseButtonContainer);
 
         var CloseIcon = document.createElement("img");
         CloseIcon.className = "CloseIcon";
         CloseIcon.src = "/Icons/CloseIcon.png";
         CloseButton.appendChild(CloseIcon);
+        CloseButtonContainer.appendChild(CloseButton);
+        
+        var CommentsContainer = document.createElement("div");
+        CommentsContainer.className = "CommentContainer";
+        CommentsContainer.id = "CommentContainer-" + PostId;
+        newDiv.appendChild(CommentsContainer);
     }
    
 }
@@ -25,8 +35,8 @@ function ShoutCommentBlock(ElementId) {
     var CommentBlock = document.getElementById(ElementId);
     CommentBlock.remove();
 }
-function AddComment(CommentBlockId, CreatorName, Content) {
-    var CommentBlock = document.getElementById(CommentBlockId);
+function AddComment(PostId, CreatorName, Content) {
+    var CommentBlock = document.getElementById("CommentContainer-" + PostId);
     var CommentDiv = document.createElement("div");
     var CreatorNameDiv = document.createElement("div");
     var ContentDiv = document.createElement("div");
