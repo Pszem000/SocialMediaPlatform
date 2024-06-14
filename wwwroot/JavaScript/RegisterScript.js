4b578c0025acdf0f51c2a92fdef304493e912134
Przemek <przemekchelski@gmail.com>
2024-06-14 14:43:29 +0200

    fixed bugs

---
+++ wwwroot/JavaScript/RegisterScript.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    var FileInput = document.getElementById("CustomFiles");
+    FileInput.addEventListener('change', function () {
+        var fileName = FileInput.files[0];
+        var label = document.getElementById("CustomFile-Label");
+        label.innerText = "File is seleced";
+    });
+});
