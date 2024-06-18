1d5ef679d7c2e3e264dda74ffb12df774d182caa
Przemek <przemekchelski@gmail.com>
2024-06-18 11:14:25 +0200

    Tried add changing BIO

---
+++ Controllers/BioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMediaPlatform.Services.Interfaces;
+
+namespace SocialMediaPlatform.Controllers
+{
+	public class BioController : Controller
+	{
+		private readonly AppDbContext _Context;
+		private readonly IUserGetter _UserGetter;
+		public BioController(AppDbContext Context,IUserGetter UserGetter)
+		{
+			_Context = Context;
+			_UserGetter = UserGetter;
+		}
+		[HttpPost]
+		public async Task ChangeBio(string Bio,string UserId)
+		{
+			var User = await _UserGetter.GetUserById(UserId);
+			User.Bio = Bio;
+			await _Context.SaveChangesAsync();
+		}
+	}
+}
--- wwwroot/JavaScript/ProfilePageScript.js
+++ wwwroot/JavaScript/ProfilePageScript.js
@@ -5,3 +5,41 @@
         button.innerText = Value;
     }
 }
+function GenerateBIOForm(BIOContent,UserId) {
+    var BIOContainer = document.getElementById("UserBio-Conatiner");
+    var FormContainer = document.createElement("div");
+    BIOContainer.appendChild(FormContainer);
+    var ChangeBioButton = document.getElementById("ChangeBioButton");
+    ChangeBio.hidden = false;
+    var BioParagarph = document.getElementById("UserBio");
+    BioParagarph.hidden = true;
+    var InputBio = document.createElement("input");
+    InputBio.type = "text";
+    InputBio.textContent = BIOContent;
+    InputBio.id = 'InputBio';
+
+    var Button = document.createElement("button");
+    Button.onclick = ChangeBio(UserId);
+    Button.textContent = "Change Bio";
+    FormContainer.append(InputBio);
+    FormContainer.append(Button);
+ 
+}
+function ChangeBio(UserId) {
+    var BioInput = document.getElementById("InputBio");
+    if (BioInput != null)
+    {
+        var BioContent = BioInput.value; 
+        var BioParagarph = document.getElementById("UserBio");
+        BioParagarph.textContent = BioContent;
+        fetch(`/Bio/ChangeBio?Bio=${BioContent}&UserId=${UserId}`);
+        RemoveInupt();
+    }
+}
+function RemoveInupt()
+{
+    var InputBio = document.getElementById("InputBio");
+    InputBio.remove();
+    var BioParagarph = document.getElementById("UserBio");
+    BioParagarph.hidden = false;
+}
