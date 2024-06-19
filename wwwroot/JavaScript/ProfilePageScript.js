cd83b3d852940496baa408bbfefedfd3f72bfcce
Przemek <przemekchelski@gmail.com>
2024-06-19 14:57:48 +0200

    fixed changing BIO

--- wwwroot/JavaScript/ProfilePageScript.js
+++ wwwroot/JavaScript/ProfilePageScript.js
@@ -9,37 +9,42 @@ function GenerateBIOForm(BIOContent,UserId) {
     var BIOContainer = document.getElementById("UserBio-Conatiner");
     var FormContainer = document.createElement("div");
     BIOContainer.appendChild(FormContainer);
-    var ChangeBioButton = document.getElementById("ChangeBioButton");
-    ChangeBio.hidden = false;
+    var ChangeBioButton = document.getElementById("ChangeBioButtonIcon");
+    ChangeBioButton.hidden = true;
     var BioParagarph = document.getElementById("UserBio");
     BioParagarph.hidden = true;
-    var InputBio = document.createElement("input");
+    var InputBio = document.createElement("textarea");
     InputBio.type = "text";
-    InputBio.textContent = BIOContent;
-    InputBio.id = 'InputBio';
-
+    InputBio.value = BIOContent;
+    InputBio.id = "InputBio";
+    InputBio.maxLength = 200;
     var Button = document.createElement("button");
-    Button.onclick = ChangeBio(UserId);
+    Button.id = "ChangeBioButton";
+    Button.onclick = function () {
+        var BioInput = document.getElementById("textarea");
+        if (BioInput != null) {
+            var BioContent = BioInput.value;
+            var BioParagarph = document.getElementById("UserBio");
+            BioParagarph.textContent = BioContent;
+            fetch(`/Bio/ChangeBio?Bio=${BioContent}&UserId=${UserId}`, {
+                method: "POST"
+            });
+            RemoveInupt();
+        }
+    }
     Button.textContent = "Change Bio";
     FormContainer.append(InputBio);
     FormContainer.append(Button);
- 
 }
-function ChangeBio(UserId) {
-    var BioInput = document.getElementById("InputBio");
-    if (BioInput != null)
-    {
-        var BioContent = BioInput.value; 
-        var BioParagarph = document.getElementById("UserBio");
-        BioParagarph.textContent = BioContent;
-        fetch(`/Bio/ChangeBio?Bio=${BioContent}&UserId=${UserId}`);
-        RemoveInupt();
-    }
-}
+
 function RemoveInupt()
 {
     var InputBio = document.getElementById("InputBio");
     InputBio.remove();
     var BioParagarph = document.getElementById("UserBio");
     BioParagarph.hidden = false;
+    var ChangeBioButtonIcon = document.getElementById("ChangeBioButtonIcon");
+    ChangeBioButtonIcon.hidden = false;
+    var ChangeBioButton = document.getElementById("ChangeBioButton");
+    ChangeBioButton.remove();
 }
